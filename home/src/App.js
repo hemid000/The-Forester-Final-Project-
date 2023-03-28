3ced4f40975420d6d3b99a3f1416071c846369aa
aslanovgx <aslanov7509@gmail.com>
2023-03-28 21:51:55 +0400

    Structure is created

---
+++ home/src/App.js
@@ -0,0 +1,11 @@
+import Forester from "./components/Pages/Home/Forester/Forester";
+
+function App() {
+  return (
+    <div className="App">
+      <Forester />
+    </div>
+  );
+}
+
+export default App;
---
+++ home/src/components/Pages/Home/Forester/Forester.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./../Forester/Forester.scss";
+import background_img from "./../../../../assets/bg-1.jpg";
+
+const Forester = () => {
+  return (
+    <section
+      id="forester"
+      style={{ backgroundImage: `url(${background_img})` }}
+    >
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">sa</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Forester;
