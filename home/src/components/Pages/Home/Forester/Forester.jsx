e7af4b223aa445f46f88b9ba3cefe490c42018b3
aslanovgx <aslanov7509@gmail.com>
2023-03-29 14:52:21 +0400

    Forester section is completed

--- home/src/components/Pages/Home/Forester/Forester.jsx
+++ home/src/components/Pages/Home/Forester/Forester.jsx
@@ -10,7 +10,20 @@ const Forester = () => {
     >
       <div className="container">
         <div className="row">
-          <div className="col-lg-12">sa</div>
+          <div className="col-lg-12">
+            <div className="forester_header">
+              <h5>Greetings, We Are</h5>
+              <h1>FORESTER</h1>
+              <h4>
+                Unordinary digital agency crafting sophisticated and
+                <br />
+                eccentric stuff that will leave you speechless
+              </h4>
+              <div className="explore_now">
+                <a href="#">explore now</a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
