8a95fd41fbef37a9d3fde8563d442cad5dace164
aslanovgx <aslanov7509@gmail.com>
2023-04-28 17:03:07 +0400

    Configuration is created

---
+++ branding/src/App.js
@@ -0,0 +1,11 @@
+import Branding from "./components/Pages/Branding/Branding";
+
+function App() {
+  return (
+    <div className="App">
+      <Branding />
+    </div>
+  );
+}
+
+export default App;
