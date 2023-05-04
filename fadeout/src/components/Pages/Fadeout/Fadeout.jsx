b0352b5d6231f59399126475c4e327f95e29fc72
aslanovgx <aslanov7509@gmail.com>
2023-05-04 14:11:53 +0400

    Filter section and filter action are completed

--- fadeout/src/components/Pages/Fadeout/Fadeout.jsx
+++ fadeout/src/components/Pages/Fadeout/Fadeout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Our from "./Our/Our";
+import Filter from "./Filter/Filter";
 
 const Fadeout = () => {
   return (
     <>
       <Our />
+      <Filter />
     </>
   );
 };
---
+++ fadeout/src/components/Pages/Fadeout/Filter/Filter.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import "./Filter.scss";
+import { IoClose } from "react-icons/io5";
+import Filter_cards from "../../../../Mock/Filter/Filter_cards";
+
+const Filter = () => {
+  const [items, setItems] = useState(Filter_cards);
+  const filterItem = (categItem) => {
+    const updatedItems = Filter_cards.filter((curElem) => {
+      return curElem.category === categItem;
+    });
+
+    setItems(updatedItems);
+  };
+
+  return (
+    <section id="gallery">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="gallery_header">
+              <h5>our portfolio</h5>
+              <h2>Gallery</h2>
+            </div>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-lg-4">
+            <ul className="gallery_lists">
+              <li onClick={() => setItems(Filter_cards)}>all</li>
+              <li onClick={() => filterItem("branding")}>branding</li>
+              <li onClick={() => filterItem("digital")}>digital</li>
+              <li onClick={() => filterItem("print")}>print</li>
+            </ul>
+          </div>
+        </div>
+        <div className="row g-2">
+          {items &&
+            items.map((el) => (
+              <div className="col-lg-6" key={`gallery_id${el.id}`}>
+                <div className="gallery_card">
+                  <img src={el.img} alt="" />
+                  <div className="gallery_details">
+                    <div className="gallery_desc">
+                      <IoClose />
+                      <h3>{el.name}</h3>
+                      <h4>{el.desc}</h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Filter;
