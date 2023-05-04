0251fbd5dbbf2fe4e9268f682f258d468079fad3
aslanovgx <aslanov7509@gmail.com>
2023-05-04 13:06:20 +0400

    Location section is completed

--- contact/src/components/Pages/Contact/Contact.jsx
+++ contact/src/components/Pages/Contact/Contact.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Cont from "./Cont/Cont";
 import Touch from "./Touch/Touch";
+import Location from "./Location/Location";
 const Contact = () => {
   return (
     <>
       <Cont />
       <Touch />
+      <Location />
     </>
   );
 };
