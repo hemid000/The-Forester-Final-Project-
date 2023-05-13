62c906382692326ddf8e2bdd6b47973ce0b9b25b
aslanovgx <aslanov7509@gmail.com>
2023-05-13 17:01:24 +0400

    Router is ready for this project

--- home/src/App.js
+++ home/src/App.js
@@ -1,22 +1,18 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/Pages/Home/Home";
 import Contact from "./components/Pages/Contact/Contact";
-import route from './router'
-import React from 'react';
+import route from "./router";
+import React from "react";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          {
-            route.map((el) => (
-              <React.Fragment key={el.path}>
-                <Route path={`${el.path}`} element={el.component} />
-              </React.Fragment>
-            ))
-          }
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        {route.map((el) => (
+          <React.Fragment key={el.path}>
+            <Route path={`${el.path}`} element={el.component} />
+          </React.Fragment>
+        ))}
+      </Routes>
     </div>
   );
 }
--- home/src/index.js
+++ home/src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Layout from "./components/Layout/Layout";
@@ -6,16 +7,12 @@ import Layout from "./components/Layout/Layout";
 import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./../node_modules/bootstrap/dist/js/bootstrap";
 import "./../node_modules/bootstrap/dist/js/bootstrap.bundle";
-import Home from "./components/Pages/Home/Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <Layout>
-      <App>
-        <Home />
-      </App>
-
+      <App />
     </Layout>
-  </React.StrictMode>
+  </BrowserRouter>
 );
